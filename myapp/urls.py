1b6f5f17d9af88bd8a37c70d92b1f3b75b27fba8
usermohit284 <mohitsaini22102003@gmail.com>
2025-07-11 15:11:39 +0530

    Added Puja page and routing

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views import home  
 from .views import services_view
+from .views import puja_view
 
 urlpatterns = [
     path('', home, name='home'), 
     path('services/', services_view, name='services'),
+    path('puja/', puja_view, name='puja'),
+
 ]
 
 
--- myapp/views.py
+++ myapp/views.py
@@ -2,9 +2,21 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html')  # Make sure 'index.html' is inside templates
+    return render(request, 'index.html')  
 
 from django.shortcuts import render
 
 def services_view(request):
     return render(request, 'services.html')
+
+from django.shortcuts import render
+
+def puja_view(request):
+    return render(request, 'puja.html') 
+
+def puja_details(request):
+    return render(request, 'puja-details.html')  
+
+
+
+
